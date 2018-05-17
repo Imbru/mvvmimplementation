776042b38a42f5dd67a8c6fd16bf620e8f173b5e
Kevin ABRIOUX <kevin.abrioux@gmail.com>
2018-05-17 17:57:12 +0200

    improve manager

--- app/src/main/java/com/kevinabrioux/mvvmimplementation/manager/LightManager.kt
+++ app/src/main/java/com/kevinabrioux/mvvmimplementation/manager/LightManager.kt
@@ -12,32 +12,31 @@ import android.os.HandlerThread
  *
  * Created by kevinabrioux on 26/01/2018.
  */
-    class LightManager(context: Context) : SensorEventListener {
+class LightManager(context: Context) : SensorEventListener {
 
-        private val sensorManager: SensorManager?
-        private val lightSensor: Sensor?
-        private var handlerThread: HandlerThread? = null
+    private val sensorManager: SensorManager? = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager?
+    private val lightSensor: Sensor?
+    private var handlerThread: HandlerThread? = null
 
-        init {
-            this.sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager?
-            this.lightSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_LIGHT)
-            this.handlerThread = HandlerThread(LightManager::class.java.simpleName)
-            this.handlerThread?.start()
-            val handler = Handler(this.handlerThread?.looper)
-            this.sensorManager?.registerListener(this, this.lightSensor, SensorManager.SENSOR_DELAY_NORMAL, handler)
-        }
-
-        fun onStop() {
-            sensorManager?.unregisterListener(this)
-            if (this.handlerThread?.isAlive == true)
-                this.handlerThread?.quitSafely()
-        }
-
-        override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        }
-
-        override fun onSensorChanged(p0: SensorEvent?) {
-            //this.listener?.onLightChange(p0?.values?.get(0)?.toInt())
-        }
-
+    init {
+        this.lightSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_LIGHT)
+        this.handlerThread = HandlerThread(LightManager::class.java.simpleName)
+        this.handlerThread?.start()
+        val handler = Handler(this.handlerThread?.looper)
+        this.sensorManager?.registerListener(this, this.lightSensor, SensorManager.SENSOR_DELAY_NORMAL, handler)
     }
+
+    fun onStop() {
+        sensorManager?.unregisterListener(this)
+        if (this.handlerThread?.isAlive == true)
+            this.handlerThread?.quitSafely()
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+        //this.listener?.onLightChange(p0?.values?.get(0)?.toInt())
+    }
+
+}
